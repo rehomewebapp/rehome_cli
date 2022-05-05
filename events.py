89fa3fee082652b36abd8ee30e656c2215147597
suubi <67806224+ssuubi@users.noreply.github.com>
2022-05-05 17:12:01 +0200

    add: event "unemployed", fix: remove event_state in saharaSand event

--- events.py
+++ events.py
@@ -2,7 +2,8 @@ import random
 # These events can happen randomly during the obersvation period.
 
 # add new event (the function name) to this list to register it.
-events = ['nothing', 'inherit', 'nothing', 'sarscov', 'nothing', 'saharaSand'] 
+events = ['nothing', 'inherit', 'nothing', 'sarscov', 'nothing', 'saharaSand', 'nothing', 'unemployed'] 
+
 
 
 def nothing(year, user, building, system, event_states):
@@ -31,14 +32,33 @@ def sarscov(year, user, building, system, event_states):
     building.ventilation_rate = building.ventilation_rate + 0.2
     event_states['sarscov'] = year + 2
 
-def saharaSand(year,user, building, system, event_states):
+def saharaSand(year, user, building, system, event_states):
     # this event reduces the efficiency of the pv module
+
+    # check if efficiency has to be reset
     if 'saharaSand' in event_states:
         if year == event_states['saharaSand']:
             system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param + 0.2
-            print(f"Your PV modules are clean again. Resetting efficiency back to {system.Photovoltaic.efficiency_param}")
+            print(f"Your PV modules are clean again. Resetting efficiency back to {system.Photovoltaic.efficiency_param}.")
+            event_states.pop('saharaSand', None) # remove event from event_states dictionary
         return
 
     print(f"Wooow. Everything is covered by sand from Sahara desert - your PV modules operate with reduced efficiency for one year: {system.Photovoltaic.efficiency_param} -> {system.Photovoltaic.efficiency_param - 0.3:.2}")
     system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param - 0.2
     event_states['saharaSand'] = year + 1
+
+def unemployed(year, user, building, system, event_states):
+    # this event reduces the sallary
+
+    # check if monthly sallary has to be reset
+    if 'unemployed' in event_states:
+        if year == event_states['unemployed']:
+            user.monthly_sallary = user.monthly_sallary * 1.5
+            print(f"Congratulations! You found a new job. Your monthly sallary is {user.monthly_sallary} euro.")
+            event_states.pop('unemployed', None) # remove event from event_states dictionary
+        return
+
+    print(f"Unfortunately you've lost your job. You receive unemployment benefits in the amount of {0.5 * user.monthly_sallary} euro/month.")
+    user.monthly_sallary = user.monthly_sallary * 0.5
+    event_states['unemployed'] = year + random.randint(1,2)
+    
