762af2674b05d9548e2a93a0da468e4889992c40
suubi <67806224+ssuubi@users.noreply.github.com>
2022-05-06 14:15:30 +0200

    add: balconyPV, coldYear, homeOffice, sealingTape, edit: use of variables instead of hard coded values

--- events.py
+++ events.py
@@ -2,9 +2,10 @@ import random
 # These events can happen randomly during the obersvation period.
 
 # add new event (the function name) to this list to register it.
-events = ['nothing', 'inherit', 'nothing', 'sarscov', 'nothing', 'saharaSand', 'nothing', 'unemployed',
-          'nothing', 'promotion', 'nothing', 'fridge', 'nothing', 'gasBoilerAging'] 
-
+#events = ['nothing', 'inherit', 'nothing', 'sarscov', 'nothing', 'saharaSand', 'nothing', 'unemployed',
+#          'nothing', 'promotion', 'nothing', 'fridge', 'nothing', 'gasBoilerAging', 'nothing', 'balconyPV',
+#          'nothing', 'coldYear', 'nothing', 'homeOffice', 'nothing', 'sealingTape'] 
+events = ['nothing', 'sealingTape']
 
 def nothing(year, user, building, system, event_states):
     # The nothing happens event corresonds to a very boring year
@@ -18,48 +19,52 @@ def inherit(year, user, building, system, event_states):
 
 def sarscov(year, user, building, system, event_states):
     # This event increases the ventilation rate of your building
+    delta_ventilation = 0.2
+    duration = random.randint(1,3)
 
     # check if ventilation rate has to be reset
     if 'sarscov' in event_states:
         if year == event_states['sarscov']:
-            building.ventilation_rate = building.ventilation_rate - 0.2
+            building.ventilation_rate = building.ventilation_rate - delta_ventilation
             print(f"Yeey, the pandemic is over. Resetting ventilation rate back to {building.ventilation_rate:.2} 1/h.")
             event_states.pop('sarscov', None) # remove event from event_states dictionary
 
         return
 
-    print(f"Huugh yet another virus! Increasing the ventilation rate for two years: {building.ventilation_rate} 1/h -> {building.ventilation_rate + 0.2:.2} 1/h.")
-    building.ventilation_rate = building.ventilation_rate + 0.2
-    event_states['sarscov'] = year + 2
+    print(f"Huugh yet another virus! Increasing the ventilation rate: {building.ventilation_rate} 1/h -> {building.ventilation_rate + delta_ventilation:.2} 1/h.")
+    building.ventilation_rate = building.ventilation_rate + delta_ventilation
+    event_states['sarscov'] = year + duration
 
 def saharaSand(year, user, building, system, event_states):
     # this event reduces the efficiency of the pv module
+    delta_efficiency = 0.2
 
     # check if efficiency has to be reset
     if 'saharaSand' in event_states:
         if year == event_states['saharaSand']:
-            system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param + 0.2
+            system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param + delta_efficiency
             print(f"Your PV modules are clean again. Resetting efficiency back to {system.Photovoltaic.efficiency_param}.")
             event_states.pop('saharaSand', None) # remove event from event_states dictionary
         return
 
-    print(f"Wooow. Everything is covered by sand from Sahara desert - your PV modules operate with reduced efficiency for one year: {system.Photovoltaic.efficiency_param} -> {system.Photovoltaic.efficiency_param - 0.3:.2}")
-    system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param - 0.2
+    print(f"Wooow. Everything is covered by sand from Sahara desert - your PV modules operate with reduced efficiency for one year: {system.Photovoltaic.efficiency_param} -> {system.Photovoltaic.efficiency_param - delta_efficiency:.2}")
+    system.Photovoltaic.efficiency_param = system.Photovoltaic.efficiency_param - delta_efficiency
     event_states['saharaSand'] = year + 1
 
 def unemployed(year, user, building, system, event_states):
     # this event reduces the sallary
+    benefit_rate = 0.5
 
     # check if monthly sallary has to be reset
     if 'unemployed' in event_states:
         if year == event_states['unemployed']:
-            user.monthly_sallary = user.monthly_sallary * 1.5
+            user.monthly_sallary = user.monthly_sallary * (1 + benefit_rate)
             print(f"Congratulations! You found a new job. Your monthly sallary is {user.monthly_sallary} euro.")
             event_states.pop('unemployed', None) # remove event from event_states dictionary
         return
 
-    print(f"Unfortunately you've lost your job. You receive unemployment benefits in the amount of {0.5 * user.monthly_sallary} euro/month.")
-    user.monthly_sallary = user.monthly_sallary * 0.5
+    print(f"Unfortunately you've lost your job. You receive unemployment benefits in the amount of {benefit_rate * user.monthly_sallary} euro/month.")
+    user.monthly_sallary = user.monthly_sallary * benefit_rate
     event_states['unemployed'] = year + random.randint(1,2)
     
 
@@ -81,16 +86,67 @@ def fridge(year, user, building, system, event_states):
 
 def gasBoilerAging(year, user, building, system, event_states):
     # this event reduces the gas boiler efficiency for one year and creates cost after one year
+    delta_efficiency = 0.1
 
-    # check if user is not unemployed in the simulated year
+    # check if efficiency has to be reset
     if 'gasBoilerAging' in event_states:
-        cost = 1000 # [euro]
-        system.GasBoiler.efficiency = system.GasBoiler.efficiency + 0.1
-        user.bank_deposit = user.bank_deposit - cost # [euro]
-        print(f"A mechanic repairs your gas boiler for {cost} euro. Resetting efficiency back to {system.GasBoiler.efficiency}.")
-        event_states.pop('gasBoilerAging', None) # remove event from event_states dictionary
+        if year == event_states['unemployed']:
+            cost = 1000 # [euro]
+            system.GasBoiler.efficiency = system.GasBoiler.efficiency + delta_efficiency
+            user.bank_deposit = user.bank_deposit - cost # [euro]
+            print(f"A mechanic repairs your gas boiler for {cost} euro. Resetting efficiency back to {system.GasBoiler.efficiency}.")
+            event_states.pop('gasBoilerAging', None) # remove event from event_states dictionary
         return
 
-    print(f"Uh-oh! The efficiency of the gas boiler drops from {system.GasBoiler.efficiency} to {system.GasBoiler.efficiency-0.1:.2} due to aging effects.")
-    system.GasBoiler.efficiency = system.GasBoiler.efficiency - 0.1
+    print(f"Uh-oh! The efficiency of the gas boiler drops from {system.GasBoiler.efficiency} to {system.GasBoiler.efficiency-delta_efficiency:.2} due to aging effects.")
+    system.GasBoiler.efficiency = system.GasBoiler.efficiency - delta_efficiency
     event_states['gasBoilerAging'] = year + random.randint(1,2)
+
+def balconyPV(year, user, building, system, event_states):
+    # this event increases your installed PV power
+    delta_power = 0.6
+
+    print(f"Happy Birthday! Your friends surprise you with a balcony power plant. Your installed PV power increases: {system.Photovoltaic.power_nom} kWp -> {system.Photovoltaic.power_nom + delta_efficiency:.2} kWp")
+    system.Photovoltaic.power_nom = system.Photovoltaic.power_nom + delta_power
+
+def coldYear(year, user, building, system, event_states):
+    # this event increases the set point temperature (instead of the ambient temperature) for one year
+    temp_amb_drop = 3 # [degC]
+
+    # check if temperature has to be reset
+    if 'coldYear' in event_states:
+        if year == event_states['coldYear']:
+            user.comfort_temperature = user.comfort_temperature - temp_amb_drop
+            print(f"Ambient temperature back at normal level.")
+            event_states.pop('coldYear', None) # remove event from event_states dictionary
+        return
+
+    print(f"Brrr. It's cold out there. This year the ambient temperature is in average {temp_amb_drop} degC cooler than usual.")
+    user.comfort_temperature = user.comfort_temperature + temp_amb_drop
+    event_states['coldYear'] = year + 1
+
+def homeOffice(year, user, building, system, event_states):
+    # this event increases the set point temperature and the electricity demand
+    delta_temp = 2 #[degC]
+    delta_el = 500 # [kWh/a]
+
+    # check if temperature and annual electricity demand has to be reset
+    if 'homeOffice' in event_states:
+        if year == event_states['homeOffice']:
+            user.comfort_temperature = user.comfort_temperature - delta_temp
+            user.annual_el_demand = user.annual_el_demand - delta_el
+            print(f"Back to office. Annual electricity demand and set point temperature back to {user.annual_el_demand} kWh and {user.comfort_temperature} degC.")
+            event_states.pop('homeOffice', None) # remove event from event_states dictionary
+        return
+    
+    duration = random.randint(1,3)
+    print(f"You work in homeoffice for {duration} years. Your annual electricity demand and the set point temperature of your heating system increase: {user.annual_el_demand} kWh -> {user.annual_el_demand + delta_el} kWh and {user.comfort_temperature} degC -> {user.comfort_temperature + delta_temp} degC")
+    user.comfort_temperature = user.comfort_temperature + delta_temp
+    user.annual_el_demand = user.annual_el_demand + delta_el
+    event_states['homeOffice'] = year + duration
+
+def sealingTape(year, user, building, system, event_states):
+    # this event reduces the infiltration rate
+    delta_infiltration = 0.1
+    print(f"A friend applies sealing tape at your windows. Your infiltration/ventilation rate drops: {building.ventilation_rate} -> {building.ventilation_rate - delta_infiltration:.2}")
+    building.ventilation_rate = building.ventilation_rate - delta_infiltration
