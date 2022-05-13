142a016d2dfb4d4dab564584ad6c0064b4f64301
suubi <67806224+ssuubi@users.noreply.github.com>
2022-05-13 12:22:19 +0200

    fixes #21

--- building.py
+++ building.py
@@ -26,19 +26,46 @@ class Building:
                 print(f"    - {key}, {value}")
             setattr(self, key, value)
 
-        # calculate additional building parameters
-        self.calc_bldg_params()
+        # calculate building geometry
+        self.calc_geometry()
 
         # load location dependend weather data
         self.weather = self.get_weather()
 
-    def calc_bldg_params(self):
-        '''This function calculates dimensions, areas and volume of the building'''
+    def calc_geometry(self):
+        '''This function calculates dimensions, areas and volume of the building
+        according to the area-estimation-method by IWU 2005
+        and own assumptions'''
         # Parameters for area estimation method according to IWU 2005
-        WINDOW_FACTOR = 0.2 # [m^2 window area /m^2 heated living area]
+        f_basement = {'none' : 0, 'unheated' : 0, 'partly heated' : 0.5, 'fully heated' : 1} # partly heating factor basement
+        f_attic = {'none' : 0, 'unheated' : 0, 'partly heated' : 0.5, 'fully heated' : 1} # partly heating factor attic 
+        f_dormers = {'none' : 1, 'existing' : 1.3} # correction factor for dormers
+        p_ground = 1.33 # [m^2 ground area / m^2 heated area per story]
+        p_roof = {'none' : 1.33, 'unheated' : 0, 'partly heated' : 0.75, 'fully heated' : 1.5} # [m^2 roof area / m^2 heated area per story]
+        p_upper_ceilig = {'none' : 0, 'unheated' : 1.33, 'partly heated' : 0.67, 'fully heated' : 0} # [m^2 upper ceiling area / m^2 heated area per story]
+        p_facade = {'compact' : 0.66, 'elongated' : 0.8} # [m^2 facade area / m^2 heated area per story]
+        p_window = 0.2 # [m^2 window area /m^2 heated living area]
+        q_facade = {'detached' : 50, 'semi-detached' : 30, 'terraced' : 10} # [m^2] additional area per story
+        h_story_nom = 2.5 # [m] nominal story height
+
+        # Calculations according to IWU
+        self.n_heated_stories = f_basement[self.basement] + self.stories + 0.75 * f_attic[self.attic] # arithmetic number of heated stories
+        self.area_story_heated = self.area_floor / self.n_heated_stories # [m^2] heated area per story
+        self.area_ground = p_ground * self.area_story_heated # [m^2]
+        self.area_roof = f_dormers[self.dormers] * p_roof[self.attic] * self.area_story_heated # [m^2]
+        self.area_upper_ceiling = p_upper_ceilig[self.attic] * self.area_story_heated # [m^2]
+        self.area_facade_story = self.story_height / h_story_nom * (p_facade[self.shape] * self.area_story_heated + q_facade[self.neighbours]) #[m^2] facade area per story
+        self.area_window = p_window * self.area_floor # [m^2]
+        self.area_basement_wall = 0.5 * f_basement[self.basement] * self.area_facade_story # [m^2] basement wall areas agains soil / unheated basement
+        self.area_facade = self.n_heated_stories * self.area_facade_story - self.area_basement_wall - self.area_window # [m^2] total opaque facade area
+        self.volume_heated = 4 * self.area_floor * self.story_height / h_story_nom # [m^3]
+        self.volume_air = self.area_floor * h_story_nom # [m^3]
+
+
 
         self.facade_orientations = np.array([0,90,180,-90]) + self.orientation_offset # [deg]
 
+        '''
         # dimensions
         self.side_length = math.sqrt(self.ground_area) # [m] assuming quadratic ground shape
         self.height = self.stories * self.story_height # [m] height of stories in total (cubic shape)
@@ -54,7 +81,7 @@ class Building:
         # volume
         # assuming fully heated attic
         self.volume = self.ground_area * self.height + 0.5 * self.side_length * self.roof_height # [m^3]
-
+        '''
 
     def get_weather(self):
         # If there is no weather data for the given location in the input directory download it
@@ -101,23 +128,39 @@ class Building:
     def calc(self, user):
         '''calculate the annual heat demand of the building
         '''
+        # parameters describing the heating state of attic and basement (based on IWU f_attic / f_basement)
+        x_basement = {'none' : 1, 'unheated' : 0, 'partly heated' : 0.5, 'fully heated' : 1} # transmission factor basement
+        x_attic = {'none' : 1, 'unheated' : 0, 'partly heated' : 0.5, 'fully heated' : 1} # transmission factor attic 
 
         dT = user.set_point_temperature - self.weather['T_amb [degC]'] # [degC] temperature difference between ambient and indoor temperature
         dT_ground = user.set_point_temperature - self.weather['T_ground [degC]'] # [degC] temperature difference between ground and indoor temperature
 
-        ventilation_losses  = self.ventilation_rate * self.volume * C.DENSITY_AIR * dT # [W]
-        infiltration_losses  = self.infiltration_rate * self.volume * C.DENSITY_AIR * dT # [W]
+        # temperatures of unheated adjacent rooms according to DIN 12831-1 Table 4
+        temp_adj = {'<1979' : 12, '1980-1995' : 14, '>1995' : 16}
+        f_corr2ext = (user.set_point_temperature - temp_adj[self.bac]) / dT # [-] temperature correction factor against ambient
+        f_corr2ground = (user.set_point_temperature - temp_adj[self.bac]) / dT_ground # [-] temperature correction factor against ground
+    
+        ventilation_losses  = self.ventilation_rate * self.volume_heated * C.DENSITY_AIR * dT # [W]
+        infiltration_losses  = self.infiltration_rate * self.volume_heated * C.DENSITY_AIR * dT # [W]
 
-        transmission_losses_wall = self.u_value_wall * self.opaque_wall_area * dT # [W]
-        transmission_losses_roof = self.u_value_roof * self.opaque_roof_area * dT # [W]
-        transmission_losses_ground = self.u_value_groundplate * self.ground_area * dT_ground # [W]
-        transmission_losses_window = self.u_value_window * self.window_area * dT # [W]
-        transmission_losses = transmission_losses_wall + transmission_losses_roof + transmission_losses_ground + transmission_losses_window # [W]
+        # Transmission losses
+        # from heated space to exterior through building envelope
+        transmission_losses_wall = self.u_value_wall * self.area_facade * dT # [W]
+        transmission_losses_window = self.u_value_window * self.area_window * dT # [W]
+        transmission_losses_roof_heated = x_attic[self.attic] * (self.u_value_roof * self.area_roof + self.u_value_upper_ceiling * self.area_upper_ceiling) * dT # [W]
+        # from heated space to exterior through unheated space
+        transmission_losses_roof_unheated = (1 - x_attic[self.attic]) * (self.u_value_roof * self.area_roof + self.u_value_upper_ceiling * self.area_upper_ceiling) * dT * f_corr2ext # [W]
+        # from heated space to ground
+        transmission_losses_ground_heated = self.u_value_groundplate * x_basement[self.basement] * (self.area_ground + self.area_basement_wall) * dT_ground # [W]
+        # from heated space to ground through unheated space
+        transmission_losses_ground_unheated = self.u_value_groundplate * (1 - x_basement[self.basement]) * (self.area_ground + self.area_basement_wall) * dT_ground * f_corr2ground# [W]
+        # total
+        transmission_losses = transmission_losses_wall + transmission_losses_window + transmission_losses_roof_heated + transmission_losses_roof_unheated + transmission_losses_ground_heated + transmission_losses_ground_unheated# [W]
         
         # solar gains assuming equally distributed window areas over orientations
         solar_gains = 0 # initialization
         for orientation in self.facade_orientations:
-            solar_gain = self.g_value_window * (1 - self.shading_window) * self.weather[f'G(v,{orientation}deg) [W/(m^2)'] * self.window_area # [W]
+            solar_gain = self.g_value_window * (1 - self.shading_window) * self.weather[f'G(v,{orientation}deg) [W/(m^2)'] * self.area_window/4 # [W]
             solar_gains =+ solar_gain # [W]
 
         heatdemand = ventilation_losses + transmission_losses_wall + infiltration_losses - solar_gains # [W]
@@ -125,8 +168,8 @@ class Building:
         annual_results = {'Annual heat demand [kWh/a]': heatdemand.sum()/1000,
                           'Transmission losses [kWh/a]': transmission_losses.sum()/1000,
                           'Transmission losses wall [kWh/a]': transmission_losses_wall.sum()/1000,
-                          'Transmission losses roof [kWh/a]': transmission_losses_roof.sum()/1000,
-                          'Transmission losses ground [kWh/a]': transmission_losses_ground.sum()/1000,
+                          'Transmission losses roof [kWh/a]': transmission_losses_roof_heated.sum()/1000+transmission_losses_roof_unheated.sum()/1000,
+                          'Transmission losses ground [kWh/a]': transmission_losses_ground_heated.sum()/1000+transmission_losses_ground_unheated.sum()/1000,
                           'Transmission losses window [kWh/a]': transmission_losses_window.sum()/1000,
                           'Ventilation losses [kWh/a]': ventilation_losses.sum()/1000,
                           'Infiltration losses [kWh/a]': infiltration_losses.sum()/1000,
